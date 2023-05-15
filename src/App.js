f4b467ad857f9607f8e10dc25987e3d9c334b8c1
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-05-15 18:52:26 +0530

    Getting Data from Api to array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+const API = "https://jsonplaceholder.typicode.com/users";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const fetchUsers = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers(API);
+  }, []);
+  return <h1>React Table</h1>;
 }
 
 export default App;
